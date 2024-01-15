bdc1255d0197fec86bd343c7f0f18cafb100f5e4
ElBugs99 <jpvaldivia99@gmail.com>
2024-01-14 21:36:24 -0300

    explicacion cursores

--- UNIDAD2/explicacionCursores.sql
+++ UNIDAD2/explicacionCursores.sql
@@ -233,3 +233,92 @@ v_tot_cargas NUMBER;
 v_asig_antig NUMBER(8):=0;
 v_bono_categ NUMBER(8):=0;
 v_asig_cargas NUMBER(8):=0;
+v_com_ventas  NUMBER(8);
+v_total_hab_cf NUMBER(10);
+v_total_haberes NUMBER(10) := 0;
+v_monto_anual_ventas NUMBER(10) := 0;
+v_monto_mes_ventas NUMBER(10) := 0;
+v_obs VARCHAR2(30);
+BEGIN
+   -- Se TRUNCAN la tabla para facilitar la ejecución del bloque
+   EXECUTE IMMEDIATE('TRUNCATE TABLE HABER_MES_VENDEDOR');
+   EXECUTE IMMEDIATE('TRUNCATE TABLE DETALLE_MODIF_CATEG_VENDEDOR');
+
+   -- Se obtienen las id mínima y máxima para recorrer la tabla vendedor
+   
+   
+   --paso 2: abrir el cursor
+    OPEN C_DATOS_VENDEDOR;
+
+    --PASO 3: LEER CADA FILA DEL CURSOR
+    --OPCION 1: WHILE SIMPLE
+    FETCH C_DATOS_VENDEDOR
+   INTO v_id_vendedor,
+        v_rut_vend ,
+        v_sueldo_base,
+        v_id_categ,
+        v_annos;
+    
+
+   WHILE C_DATOS_VENDEDO%FOUND LOOP 
+   --SE LEE CADA FILA DEL CURSOR
+   
+        
+    --se debe indicar la condicion de salida del loop
+    exit when C_DATOS_VENDEDOR%NOTFOUND;
+    
+     -- Se inicializan las variables
+     v_asig_antig:=0;
+     v_bono_categ:=0;
+     v_asig_cargas:=0;
+     -- Obtiene los datos básicos para el proceso
+      
+      -- Calcula bonificación por antiguedad
+      IF v_annos > 0 THEN
+           SELECT porc_bonif
+             INTO v_porc_antig
+             FROM bonificacion_antig
+            WHERE v_annos BETWEEN anno_tramo_inf AND anno_tramo_sup;
+            v_asig_antig:=ROUND(v_sueldo_base*(v_porc_antig/100));
+      END IF;
+
+     -- Cálculo monto de comisiones del mes y año de proceso   
+     SELECT NVL(SUM(monto_venta),0) 
+     INTO v_monto_mes_ventas
+     FROM boleta
+     WHERE id_vendedor = v_id_vendedor
+       AND TO_CHAR(fecha_venta,'MM/YYYY')= SUBSTR(:b_fecha_proceso,4);
+    v_com_ventas:=ROUND(v_monto_mes_ventas*:b_porc_com_ventas);
+
+   /*Se obtiene el monto total de las ventas realizadas durante el año para actualizar la categoria
+ del vendedor */
+     SELECT NVL(SUM(monto_venta),0) 
+     INTO v_monto_anual_ventas
+     FROM boleta
+     WHERE id_vendedor = v_id_vendedor
+       AND TO_CHAR(fecha_venta,'YYYY')= SUBSTR(:b_fecha_proceso,7);
+
+     -- Cálculo bono especial por categoría 
+      IF v_id_categ IN ('A', 'B') THEN 
+           SELECT porcentaje/100
+             INTO v_porc_categ
+             FROM categoria
+            WHERE id_categoria = v_id_categ;
+            v_bono_categ:=ROUND(v_com_ventas*v_porc_categ);
+      END IF;
+
+     /* Cálculo de asignación cargar familiares */
+     -- Obtiene el total de haberes para calcular asignación familiar
+     v_total_hab_cf:= v_sueldo_base + v_bono_categ + v_asig_antig;
+     -- Si el total de haberes para cargas familiares es mayor al tope entonces el valor será cero
+     IF v_total_hab_cf > :b_haber_tope_cf THEN
+        v_asig_cargas:=0;
+     ELSE
+         -- Obtiene el numero de cargas y calcula la asignacion por cargas
+         SELECT COUNT(*)
+           INTO v_tot_cargas
+           FROM carga_familiar
+          WHERE id_vendedor = v_id_vendedor;
+          -- calcula el valor de la asignación
+          v_asig_cargas := ROUND(:b_valor_cf * v_tot_cargas);
+     END IF;    
