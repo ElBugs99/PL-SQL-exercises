08cb31e0330f1166fe7a977bba6c9bcc23105a44
ElBugs99 <jpvaldivia99@gmail.com>
2024-01-26 02:26:48 -0300

    explicacion triggers

--- UNIDAD3/explicacionTriggers.sql
+++ UNIDAD3/explicacionTriggers.sql
@@ -15,3 +15,25 @@ trigger hace que las sentencias dml sobre la tabla sean un poco mas lentas.
     
     -que se activa por cada fila afectada por la sentencia dml que se ejecuta sobre la tabla (Examen).
     se conoce como trigger asociado a filas
+    
+5 para crear un trigger asociado a filas de la tabla:
+    
+    create or replace trigger trg_nombre
+    before|after => el tiempo en que el trigger se va a ejecutar.
+            before: el trigger se va a ejecutar entes de
+            la sentencia dml que activo el trigger
+            after: el trigger se va a ejecutar despues de la sentencia
+            dml que activo el trigger
+    insert or update or delete   => sentencia dml activara la ejecucion del trigger
+    
+    on nombre_tabla => a que tabla se asocia el trigger
+    for each row => el trigger se va a ejecutar por cada fila de la tabla afectada
+                    por la sentencia dml
+    declare => es para definir variables en el trigger.
+    declaracion de variables
+    begin
+        cuerpo_del_trigger
+    exception
+        sentencias plsql y/o sql;
+    end trg_nombre;
+    
