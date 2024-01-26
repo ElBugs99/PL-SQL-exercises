f10af892b1a039fe502cc94d7368077b52377534
ElBugs99 <jpvaldivia99@gmail.com>
2024-01-26 02:25:16 -0300

    explicacion triggers

--- UNIDAD3/explicacionTriggers.sql
+++ UNIDAD3/explicacionTriggers.sql
@@ -4,3 +4,6 @@ si se elimina la tabla asociada al trigger automaticamente se elimina el trigger
 
 2. al estar asociado a una tabla se ejecutara cada vez que realice un insert / update y/o un delete
 sobre las filas de la tabla
+
+3 una tabla puede tener asociado mas de un trigger. pero hay que considerar que el
+trigger hace que las sentencias dml sobre la tabla sean un poco mas lentas.
