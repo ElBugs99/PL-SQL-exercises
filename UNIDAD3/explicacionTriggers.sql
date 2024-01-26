9268680d14583df59f1301fd80314b3271243bba
ElBugs99 <jpvaldivia99@gmail.com>
2024-01-26 02:24:30 -0300

    explicaicon triggers

---
+++ UNIDAD3/explicacionTriggers.sql
@@ -0,0 +1,6 @@
+trigger
+1. un programa plsql con nombre, que se debe asociar a una tabla. por lo tanto
+si se elimina la tabla asociada al trigger automaticamente se elimina el trigger.
+
+2. al estar asociado a una tabla se ejecutara cada vez que realice un insert / update y/o un delete
+sobre las filas de la tabla
