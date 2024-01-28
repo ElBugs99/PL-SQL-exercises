09eef1950e996ef17a17ec1a021e15204490b8f3
ElBugs99 <jpvaldivia99@gmail.com>
2024-01-28 17:58:17 -0300

    prueba3

--- UNIDAD3/prueba3Resuelta.sql
+++ UNIDAD3/prueba3Resuelta.sql
@@ -5,3 +5,11 @@ DECLARE
 v_total_haberes NUMBER(8);
 v_calif VARCHAR2(80);
 BEGIN
+v_total_haberes:=:NEW.sueldo_base +
+:NEW.asig_colacion +
+:NEW.asig_movilizacion +
+:NEW.asig_especial_ant +
+:NEW.asig_escolaridad +
+:NEW.comision_ventas;
+IF v_total_haberes BETWEEN 400000 AND 700000 THEN
+v_calif:='Total de Haberes: ' || v_total_haberes ||
