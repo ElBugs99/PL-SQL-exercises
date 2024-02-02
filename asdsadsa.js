7b3e44b9a11c1bc3f1424382770132f3df763b27
ElBugs99 <jpvaldivia99@gmail.com>
2024-02-02 01:42:31 -0300

    .

---
+++ asdsadsa.js
@@ -0,0 +1,39 @@
+//hacer una funcion que diga si los parentesis de apertura y cierre son correctos, se espera recibir un String
+
+//ejemplos de malos
+/* 
+([ )]
+)()
+(]][)
+( ) () //Extra
+*/
+
+
+
+
+//hacer un paginador, poner el numero del elemento, paginar de 2 en 2
+//ordenar nombres que tienen numeros romanos en base al numero
+//ordernar las tallas, de mayor a menor, ordenar las tallas que se entregen, pueden no ser todas
+console.log('hola');
+
+
+const ordenarTallas = ( arrayDeTallas ) => {
+
+    const tallas =
+     {
+        S: 1,
+        M: 2,
+        L: 3,
+        XL: 4,
+        XXL: 5
+     };
+
+    for (let talla in tallas) {
+
+        console.log(talla);
+
+    }
+
+    //console.log(tallas["S"]);
+
+};
